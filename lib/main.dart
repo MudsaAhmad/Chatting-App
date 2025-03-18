d113e93a236e84cff3f285cdfa6ffbbbbedee5fb
MudsaAhmad <mudassirahmad774@gmail.com>
2025-03-18 14:50:02 +0500

    project running Kotlin issue solved.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import '11-03-2025/login.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
